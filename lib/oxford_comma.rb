fd98392c3ad78666390faaff0eee4d1c350fadd1
Alex Neustein <alex.neustein@gmail.com>
2018-06-21 01:46:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
-  return array[0] if array.size == 1
-  return array[0] + " and " + array[1] if array.size == 2
+  puts array[0] if array.size == 1
+  puts array[0] + " and " + array[1] if array.size == 2
   if array.size > 2
     the_string = ""
     for i in 1..array.size do 
@@ -8,6 +8,6 @@ def oxford_comma(array)
       the_string << ", " if i < array.size
       the_string << "and " if i == (array.size - 1)
     end
-    return the_string
+    puts the_string
   end
 end
