d93ddc0b569ac4ff44d72d049d80fbf06b36974d
Alex Neustein <alex.neustein@gmail.com>
2018-06-21 01:36:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,10 @@ def oxford_comma(array)
   puts array[0] if array.size == 1
   if array.size != 1
     the_string = ""
-    
+    for i in 1..deli_name.size do 
+      line_string << i.to_s + ". " + deli_name[(i-1)]
+      line_string << " " if i < deli_name.size
+    end
   end
 end
 
